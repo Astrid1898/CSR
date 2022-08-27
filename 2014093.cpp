f43e2ad00bdbf7b2a86f6380a51897b8eb9fe573
Astrid <zhaojiangfeng1996@163.com>
2022-08-27 19:17:52 +0800

    update

--- 2014093.cpp
+++ 2014093.cpp
@@ -4,7 +4,7 @@
  * @Author: Zhao Jiangfeng
  * @Date: 2022-08-27 14:50:27
  * @LastEditors: Zhao Jiangfeng
- * @LastEditTime: 2022-08-27 14:50:52
+ * @LastEditTime: 2022-08-27 15:01:23
  */
 #include <iostream>
 
@@ -12,5 +12,6 @@ using namespace std;
 
 int main()
 {
+    int n;
     return 0;
 }
