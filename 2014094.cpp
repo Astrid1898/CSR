5e3297821070ee7329b44273bf5562a283f98854
Astrid <zhaojiangfeng1996@163.com>
2022-08-28 22:05:25 +0800

    update

---
+++ 2014094.cpp
@@ -0,0 +1,16 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Zhao Jiangfeng
+ * @Date: 2022-08-28 21:47:59
+ * @LastEditors: Zhao Jiangfeng
+ * @LastEditTime: 2022-08-28 21:48:41
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
