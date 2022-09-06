7c7b0ea106dfce283fc9a6f7bbfc9b703cc581df
Astrid <zhaojiangfeng1996@163.com>
2022-09-07 01:03:36 +0800

    update

---
+++ 2015904.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Zhao Jiangfeng
+ * @Date: 2022-09-06 14:12:46
+ * @LastEditors: Zhao Jiangfeng
+ * @LastEditTime: 2022-09-06 14:36:02
+ */
+#include <bits/stdc++.h>
+
+using namesapce std;
+
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(0), cout.tie(0);
+
+map<int, vector<int>> roads;
+
+int main()
+{
+    int n, m;
+    return 0;
+}
