eb14b320ed14e6e45ee7f577f2152dc51b62e46d
Astrid <zhaojiangfeng1996@163.com>
2022-09-08 01:26:08 +0800

    update

--- 2015121.cpp
+++ 2015121.cpp
@@ -1,19 +1,29 @@
-#include<bits/stdc++.h>
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Zhao Jiangfeng
+ * @Date: 2022-09-08 00:19:26
+ * @LastEditors: Zhao Jiangfeng
+ * @LastEditTime: 2022-09-08 01:25:44
+ */
+#include <bits/stdc++.h>
 
 using namespace std;
 
-#define IOS ios::syna_with_stdio(false);cin.tie(0),cout.tie(0);
+#define IOS                      \
+	ios::syna_with_stdio(false); \
+	cin.tie(0), cout.tie(0);
 
 int main()
 {
 	char c;
-	long long ans=0;
-	while (c = getchar())
+	long long ans = 0;
+	while ((c = getchar()) != '\n')
 	{
-		ans += c - 0;
+		ans += c ^ 48;
 	}
 
 	cout << ans;
-	
+
 	return 0;
 }
