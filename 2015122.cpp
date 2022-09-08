b72cb0a330ef301fe6f14f926cb299dd70fce73a
Astrid1898 <zhaojiangfeng1996@163.com>
2022-09-08 14:05:47 +0800

    update

---
+++ 2015122.cpp
@@ -0,0 +1,14 @@
+//
+// Created by zhaojiangfeng on 2022/9/8.
+//
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false);cin.tie(0),cout.tie(0);
+
+int main()
+{
+    IOS;
+    return 0;
+}
