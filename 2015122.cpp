1b6885853c1e9e713ab515a4fdb3ba728570d67a
Astrid1898 <zhaojiangfeng1996@163.com>
2022-09-09 14:53:22 +0800

    update

--- 2015122.cpp
+++ 2015122.cpp
@@ -10,73 +10,73 @@ using namespace std;
 int main()
 {
     IOS;
-    int n,m;
-    cin>>n>>m;
-    
+    int n, m;
+    cin >> n >> m;
+
     int checkerboard[n][m];
     bool is_erased[n][m];
-    memset(checkerboard,0,sizeof (checkerboard));
-    memset(is_erased,0,sizeof (is_erased));
+    memset(checkerboard, 0, sizeof(checkerboard));
+    memset(is_erased, 0, sizeof(is_erased));
 
-    for(int i=0;i<n;i++)
-        for(int j=0;j<m;j++)
-            cin>>checkerboard[i][j];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> checkerboard[i][j];
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        int j=0,cnt=1;
-        while(j<m)
+        int j = 0, cnt = 1;
+        while (j < m)
         {
-            if(cnt<m&&checkerboard[i][j]==checkerboard[i][cnt])
+            if (cnt < m && checkerboard[i][j] == checkerboard[i][cnt])
             {
                 cnt++;
                 continue;
             }
-            if(cnt-j>2)
+            if (cnt - j > 2)
             {
                 for (; j < cnt; j++)
                     is_erased[i][j] = true;
             }
 
-            j=cnt;
+            j = cnt;
         }
 
     }
 
-    for(int j=0;j<m;j++)
+    for (int j = 0; j < m; j++)
     {
-        int i=0,cnt=1;
-        while(i<n)
+        int i = 0, cnt = 1;
+        while (i < n)
         {
-            if(cnt<n&&checkerboard[i][j]==checkerboard[cnt][j])
+            if (cnt < n && checkerboard[i][j] == checkerboard[cnt][j])
             {
                 cnt++;
                 continue;
             }
-            else if(cnt-i>2)
+            else if (cnt - i > 2)
             {
                 for (; i < cnt; i++)
                     is_erased[i][j] = true;
             }
 
-            i=cnt;
+            i = cnt;
         }
 
     }
 
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<m;j++)
+        for (int j = 0; j < m; j++)
         {
-            if(is_erased[i][j])
-                checkerboard[i][j]=0;
-            if(j==0)
-                cout<<checkerboard[i][j];
+            if (is_erased[i][j])
+                checkerboard[i][j] = 0;
+            if (j == 0)
+                cout << checkerboard[i][j];
             else
-                cout<<" "<<checkerboard[i][j];
+                cout << " " << checkerboard[i][j];
         }
 
-        cout<<endl;
+        cout << endl;
 
     }
 
