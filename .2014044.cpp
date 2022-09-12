0691b3b41c4eabc4e6d124ea39a30f20ec373aef
Astrid <zhaojiangfeng1996@163.com>
2022-09-12 22:32:43 +0800

    update

--- .2014044.cpp
+++ .2014044.cpp
@@ -5,6 +5,9 @@
  * @Date: 2022-09-12 02:03:35
  * @LastEditors: Zhao Jiangfeng
  * @LastEditstep: 2022-09-12 02:34:04
+ *
+ * 之前会超时，后来参考别人的代码加了一个带有时间维度的visited数组
+ * 之后就不会超时了，这个数组确实起到了有效剪枝的作用
  */
 #include <bits/stdc++.h>
 
@@ -20,10 +23,11 @@ pair<int, int> danger[MAX][MAX];
 
 int n, m, t;
 int step = 0;
+bool visited[MAX][MAX][310];
 
 bool is_avaiable(int x, int y, int step)
 {
-    if (x > n || y > m || x < 1 || y < 1)
+    if (x > n || y > m || x < 1 || y < 1 || visited[x][y][step])
         return false;
 
     else if (step >= danger[x][y].first && step <= danger[x][y].second)
@@ -44,6 +48,7 @@ queue<position> q;
 void bfs()
 {
     q.push(position(1, 1, 0));
+    visited[1][1][0] = true;
     while (!q.empty())
     {
         position cur_pos = q.front();
@@ -56,18 +61,22 @@ void bfs()
         if (is_avaiable(cur_pos.x + 1, cur_pos.y, cur_pos.step + 1))
         {
             q.push(position(cur_pos.x + 1, cur_pos.y, cur_pos.step + 1));
+            visited[cur_pos.x + 1][cur_pos.y][cur_pos.step + 1] = true;
         }
         if (is_avaiable(cur_pos.x - 1, cur_pos.y, cur_pos.step + 1))
         {
             q.push(position(cur_pos.x - 1, cur_pos.y, cur_pos.step + 1));
+            visited[cur_pos.x - 1][cur_pos.y][cur_pos.step + 1] = true;
         }
         if (is_avaiable(cur_pos.x, cur_pos.y + 1, cur_pos.step + 1))
         {
             q.push(position(cur_pos.x, cur_pos.y + 1, cur_pos.step + 1));
+            visited[cur_pos.x][cur_pos.y + 1][cur_pos.step + 1] = true;
         }
         if (is_avaiable(cur_pos.x, cur_pos.y - 1, cur_pos.step + 1))
         {
             q.push(position(cur_pos.x, cur_pos.y - 1, cur_pos.step + 1));
+            visited[cur_pos.x][cur_pos.y - 1][cur_pos.step + 1] = true;
         }
     }
 }
@@ -84,6 +93,8 @@ int main()
         danger[r][c].first = a;
         danger[r][c].second = b;
     }
+
+    memset(visited, 0, sizeof(visited));
 
     bfs();
 
