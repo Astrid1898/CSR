ea07e0371de14e43b55758067568fc8da0b821a2
Astrid <zhaojiangfeng1996@163.com>
2022-09-19 01:04:56 +0800

    update

---
+++ 2022064.cpp
@@ -0,0 +1,24 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Zhao Jiangfeng
+ * @Date: 2022-09-19 01:03:23
+ * @LastEditors: Zhao Jiangfeng
+ * @LastEditTime: 2022-09-19 01:04:47
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(0), cout.tie(0);
+
+int main()
+{
+    IOS;
+
+    freopen("CSR/in.txt", "r", stdin);
+
+    return 0;
+}
