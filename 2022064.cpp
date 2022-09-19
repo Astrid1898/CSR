fc62b238edb0b4dfd2765175fba6bac77ed67fd0
Astrid <zhaojiangfeng1996@163.com>
2022-09-20 01:11:21 +0800

    update

--- 2022064.cpp
+++ 2022064.cpp
@@ -4,7 +4,7 @@
  * @Author: Zhao Jiangfeng
  * @Date: 2022-09-19 01:03:23
  * @LastEditors: Zhao Jiangfeng
- * @LastEditTime: 2022-09-19 01:06:58
+ * @LastEditTime: 2022-09-20 01:10:38
  */
 #include <bits/stdc++.h>
 
@@ -19,9 +19,19 @@ int main()
     IOS;
 
     freopen("CSR/in.txt", "r", stdin);
-    int m;
+    int m, id;
+    cin >> m;
     for (int i = 0; i < m; i++)
     {
+        cin >> id;
+        switch (id)
+        {
+        case 1:
+        {
+            int x1, y1, x2, y2 a;
+            cin >> x2 >> y1 >> x2 >> y2 >> a;
+        }
+        }
     }
 
     return 0;
