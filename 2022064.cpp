0dccfe937cbee512db0a7d0a9fb2347c6992adb1
Astrid <zhaojiangfeng1996@163.com>
2022-09-21 13:16:11 +0800

    update

--- 2022064.cpp
+++ 2022064.cpp
@@ -4,7 +4,7 @@
  * @Author: Zhao Jiangfeng
  * @Date: 2022-09-19 01:03:23
  * @LastEditors: Zhao Jiangfeng
- * @LastEditTime: 2022-09-20 01:10:38
+ * @LastEditTime: 2022-09-21 13:16:04
  */
 #include <bits/stdc++.h>
 
@@ -30,6 +30,18 @@ int main()
         {
             int x1, y1, x2, y2 a;
             cin >> x2 >> y1 >> x2 >> y2 >> a;
+            break;
+        }
+        case 2:
+        {
+            int k;
+            cin >> k;
+        }
+        case 3:
+        {
+            int x, y, d, i, t;
+            cin >> x >> y >> d >> i >> t;
+            break;
         }
         }
     }
