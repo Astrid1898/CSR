13a5a4dd5ec2e6d00a08b9add549858d6e12e88e
Astrid1898 <zhaojiangfeng1996@163.com>
2022-10-13 21:29:45 +0800

    update

--- 2022093.cpp
+++ 2022093.cpp
@@ -9,13 +9,48 @@ using namespace std;
 
 #define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
+
 int main()
 {
     IOS;
 
     freopen("in.txt", "r", stdin);
 
+
+    map<int,pair<int,int>> dangerous_area;
     int n;
     cin>>n;
+    int d,u,r,ri,mi,pij;
+    for(int i=0;i<n;i++)
+    {
+        for(auto it=dangerous_area.begin();it!=dangerous_area.end();)
+        {
+            if(it->second.second<i)
+            {
+                dangerous_area.erase(it++);
+            }
+            else
+            {
+                it++;
+            }
+        }
+
+        cin>>ri>>mi;
+        for(int j=0;j<ri;j++)
+        {
+            cin>>pij;
+            if(dangerous_area.find(pij)!=dangerous_area.end())
+            {
+                dangerous_area[pij].second=i+6;
+            }
+            else
+            {
+                dangerous_area[pij]= make_pair(i,i+6);
+            }
+
+        }
+
+    }
+
     return 0;
 }
