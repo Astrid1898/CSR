403033081b090569c37462406b156ee873331a51
Astrid1898 <zhaojiangfeng1996@163.com>
2022-10-14 21:56:51 +0800

    update

--- 2022093.cpp
+++ 2022093.cpp
@@ -18,23 +18,14 @@ int main()
 
 
     map<int,pair<int,int>> dangerous_area;
+    map<int,set<int>> dangerous_people;
     int n;
     cin>>n;
     int d,u,r,ri,mi,pij;
     for(int i=0;i<n;i++)
     {
-        for (auto it = dangerous_area.begin(); it != dangerous_area.end();)
-        {
-            if (it->second.second < i)
-            {
-                dangerous_area.erase(it++);
-            }
-            else
-            {
-                it++;
-            }
-        }
 
+        //先处理列入高风险地区的列表
         cin >> ri >> mi;
         for (int j = 0; j < ri; j++)
         {
@@ -49,15 +40,63 @@ int main()
             }
 
         }
+
+        //将前一日解除高风险，并且今日无风险信息的地区解除高风险，同时处理因访问该地区被列入高风险的用户
+        for (auto it = dangerous_area.begin(); it != dangerous_area.end();)
+        {
+            if (it->second.second < i)
+            {
+                int area=it->first;
+                for(auto it2=dangerous_people.begin();it2!=dangerous_people.end();)
+                {
+                    if(it2->second.find(area)!=it2->second.end())
+                    {
+                        it2->second.erase(area);
+                        if(it2->second.empty()){dangerous_people.erase(it2++);}
+                        else{it2++;}
+                    }
+                    else
+                    {
+                        it2++;
+                    }
+                }
+                dangerous_area.erase(it++);
+            }
+            else
+            {
+                it++;
+            }
+        }
+
+
+
+        //处理今日用户到访地区信息
         for(int j=0;j<mi;j++)
         {
             cin>>d>>u>>r;
+            if(dangerous_area.find(r)!=dangerous_area.end())
+            {
+                int begin=dangerous_area[r].first;
+                if(d>=begin)
+                {
+                    dangerous_people[u].insert(r);
+
+                }
+            }
         }
 
-        cout<<"在第 "<<i<<" 天，高风险地区一共有 "<<dangerous_area.size()<<" 个,分别是:\n";
-        for (auto it = dangerous_area.begin(); it != dangerous_area.end(); it++)
+//        cout<<"在第 "<<i<<" 天，高风险地区一共有 "<<dangerous_area.size()<<" 个,分别是: ";
+//        for (auto it = dangerous_area.begin(); it != dangerous_area.end(); it++)
+//        {
+//            cout<<it->first<<" ";
+//        }
+//        cout<<endl;
+//
+//        cout<<"在第 "<<i<<" 天, 风险名单中一共有 "<<dangerous_people.size()<<" 人，分别是: ";
+        cout<<i;
+        for(auto it=dangerous_people.begin();it!=dangerous_people.end();it++)
         {
-            cout<<it->first<<" ";
+            cout<<" "<<it->first;
         }
         cout<<endl;
 
