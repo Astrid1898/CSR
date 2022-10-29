07a726a7ec2b12ff3b6cf59b927a0c01a6202e89
Astrid <zhaojiangfeng1996@163.com>
2022-10-29 20:44:59 +0800

    update

---
+++ 2016121.cpp
@@ -0,0 +1,21 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Zhao Jiangfeng
+ * @Date: 2022-10-29 20:42:56
+ * @LastEditors: Zhao Jiangfeng
+ * @LastEditTime: 2022-10-29 20:44:28
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+int main()
+{
+    IOS;
+}
