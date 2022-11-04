2862da4911494b8fa6bf640d9b0a45c54ddf4b80
Astrid1898 <zhaojiangfeng1996@163.com>
2022-11-04 19:38:16 +0800

    lab update

--- 2016121.cpp
+++ 2016121.cpp
@@ -18,5 +18,36 @@ using namespace std;
 int main()
 {
     IOS;
+
+    freopen("D:\\zjf\\csp\\csp\\csp\\in.txt", "r", stdin);
+
+    map<int,int> num_count;
+
+    int n,tmp;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>tmp;
+        num_count[tmp]++;
+    }
+
+    int before=0,sign=0;
+
+    for(auto it=num_count.begin();it!=num_count.end();it++)
+    {
+        if(n-it->second-before==before)
+        {
+            sign=1;
+            cout<<it->first;
+            break;
+        }
+        before+=it->second;
+    }
+
+    if(sign==0)
+        cout<<-1;
+
+
+
     return 0;
 }
