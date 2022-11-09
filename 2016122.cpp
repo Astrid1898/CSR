b6fd99f7e3a2b7856327fb4a4fc9e92a74101f76
Astrid <zhaojiangfeng1996@163.com>
2022-11-09 22:00:04 +0800

    update

--- 2016122.cpp
+++ 2016122.cpp
@@ -4,7 +4,7 @@
  * @Author: Zhao Jiangfeng
  * @Date: 2022-11-05 19:57:45
  * @LastEditors: Zhao Jiangfeng
- * @LastEditTime: 2022-11-09 21:15:49
+ * @LastEditTime: 2022-11-09 21:42:59
  */
 #include <bits/stdc++.h>
 
@@ -19,7 +19,7 @@ int main()
 {
     IOS;
 
-    freopen("in.txt", "r", stdin);
+    freopen("/home/jf-z/codefield/CODE_CPP/Cpp_Single/CSR/in.txt", "r", stdin);
 
     int t, salary;
     cin >> t;
@@ -28,9 +28,22 @@ int main()
         salary = t;
     else if (t <= 4955)
     {
-        salary = (int)((t - 3000) / 0.03) + 3000;
+        salary = (int)((t - 3000) / 0.97) + 3000;
+    }
+    else if (t <= 7655)
+    {
+        salary = (int)((t - 4995) / 0.9) + 5000;
+    }
+    else if (t <= 11255)
+    {
+        salary = (int)((t - 7655) / 0.8) + 8000;
+    }
+    else if (t <= 31655)
+    {
+        salary = (int)((t - 9000) / 0.75) + 12500;
     }
 
+    salary = salary / 100 * 100;
     cout << salary << endl;
     return 0;
 }
