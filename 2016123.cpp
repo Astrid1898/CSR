9d77573b716f79d88de84f67277f423e638e0c95
Astrid1898 <zhaojiangfeng1996@163.com>
2022-11-11 22:45:00 +0800

    lab update

---
+++ 2016123.cpp
@@ -0,0 +1,18 @@
+//
+// Created by zhaojiangfeng on 2022/11/11.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    IOS;
+    freopen("./in.txt","r",stdin);
+
+    return 0;
+}
