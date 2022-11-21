c9d0c0c493e9bd26385b27e14fe198ae04d22858
Astrid <zhaojiangfeng1996@163.com>
2022-11-21 23:29:12 +0800

    update

---
+++ 2016125.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Zhao Jiangfeng
+ * @Date: 2022-11-21 23:27:36
+ * @LastEditors: Zhao Jiangfeng
+ * @LastEditTime: 2022-11-21 23:29:07
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(0);                  \
+    cout.tie(0);
+
+int main()
+{
+    IOS;
+
+    freopen("in.txt", "r", stdin);
+
+    return 0;
+}
