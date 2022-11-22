f620a35e237eb31dec0f054eea3e7869d03f1c97
Astrid <zhaojiangfeng1996@163.com>
2022-11-22 22:39:12 +0800

    update

--- 2016125.cpp
+++ 2016125.cpp
@@ -4,7 +4,7 @@
  * @Author: Zhao Jiangfeng
  * @Date: 2022-11-21 23:27:36
  * @LastEditors: Zhao Jiangfeng
- * @LastEditTime: 2022-11-21 23:29:07
+ * @LastEditTime: 2022-11-22 22:38:57
  */
 #include <bits/stdc++.h>
 
@@ -20,6 +20,9 @@ int main()
     IOS;
 
     freopen("in.txt", "r", stdin);
+
+    int n,m;
+    cin>>n>>m;
 
     return 0;
 }
