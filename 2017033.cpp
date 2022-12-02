ae1f6162f8ea37847195d7674f8d1972ee4c75ed
Astrid1898 <zhaojiangfeng1996@163.com>
2022-12-02 21:53:27 +0800

    lab update

---
+++ 2017033.cpp
@@ -0,0 +1,18 @@
+//
+// Created by zhaojiangfeng on 2022/12/1.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    IOS;
+    freopen("D:\\zjf\\csp\\csp\\csp\\in.txt", "r", stdin);
+
+    return 0;
+}
