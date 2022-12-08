da5cdfc54099b910de22e3566d6fe48c1ee317f1
Astrid1898 <zhaojiangfeng1996@163.com>
2022-12-08 22:09:24 +0800

    lab update

---
+++ 2017035.cpp
@@ -0,0 +1,18 @@
+//
+// Created by zhaojiangfeng on 2022/12/8.
+//
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+int main()
+{
+    IOS;
+    long long n,m,A,B,Q,X_0;
+    cin>>n>>m>>A>>B>>Q>>X_0;
+    return 0;
+}
