9717546d16674e3308b620e12beb6fa2822d2de8
ZhiTeng <zhaojiangfeng1996@163.com>
2023-03-10 22:53:05 +0800

    AC

--- 2017122.cpp
+++ 2017122.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 #define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+const int N = 1e4 + 10;
 
 int main()
 {
@@ -16,35 +17,40 @@ int main()
     int n, k;
     cin >> n >> k;
     int count = 0, base = 10;
-    vector<bool> status(n + 1, true);
-    int next = k;
-    int start=0;
+    bool status[N];
+    for (int i = 1; i <= n; i++)
+        status[i] = true;
+    int start = 0;
 
-    while(count!=n-1)
+    while (true)
     {
-        for(int i=1;i<=n;i++)
+        for (int i = 1; i <= n; i++)
         {
-            if(status[i])
+            if (status[i])
             {
                 start++;
-                if(start%k==0||(start-k)%10==0)
+                if (start % k == 0 || (start - k) % 10 == 0)
                 {
-                    status[i]=false;
+                    status[i] = false;
                     count++;
                 }
+                //一局比赛里可能会淘汰多人
+                if (count == n - 1)
+                    break;
             }
 
         }
+        if(count==n-1)
+            break;
     }
 
-    for(int i=1;i<=n;i++)
+    for (int i = 1; i <= n; i++)
     {
-        if(status[i])
+        if (status[i])
         {
-            cout<<i<<endl;
+            cout << i << endl;
             return 0;
         }
     }
-
     return 0;
 }
