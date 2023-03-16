2501cd724dc83e4003831b3b7bd4fde08dfae595
thinkbook <zhaojiangfeng1996@163.com>
2023-03-17 01:37:00 +0800

    未考虑疲劳度连续增加

--- 2017124.cpp
+++ 2017124.cpp
@@ -2,17 +2,48 @@
 // Created by zjf on 2023/3/16.
 //
 
+//连续走小道疲劳度会累加
+
 #include<bits/stdc++.h>
 using namespace std;
 
 #define IOS ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+typedef pair<int,int> P;//first边权，second目的顶点编号
 
-int fatigues[502][502];
+int fatigues[502][502],d[502];
+bool vis[502]={false};
+
+void dijkstra(int n)
+{
+    d[1]=0;
+    int u,v;
+    priority_queue<P,vector<P>,greater<P>> pq;
+    pq.push(make_pair(0,1));
+    while(!pq.empty())
+    {
+        P p=pq.top();
+        pq.pop();
+        v=p.second;
+        if(d[v]<p.first) continue;
+        vis[v]=1;
+        for(u=1;u<=n;++u)
+        {
+            if(!vis[u]&&d[u]>d[v]+fatigues[v][u])
+            {
+                d[u]=d[v]+fatigues[u][v];
+                pq.push(make_pair(d[u],u));
+            }
+        }
+    }
+}
+
 
 int main()
 {
     IOS;
+    freopen("D:\\code\\CODE_C\\C_Single\\CSR\\in.txt","r",stdin);
     memset(fatigues,0x3f,sizeof(fatigues));
+    memset(d,0x3f,sizeof(d));
 
     int n,m;
     cin>>n>>m;
@@ -29,5 +60,7 @@ int main()
             fatigues[b][a]=fatigue;
         }
     }
+    dijkstra(n);
+    cout<<d[n]<<endl;
     return 0;
 }
